3f11e1ec1a1c1f68ca555eb2e26698a906a40a81
Denis <denis@ontiveros.info>
2025-12-06 18:48:47 +0000

    added some comments.

--- day1_solution.py
+++ day1_solution.py
@@ -1,15 +1,23 @@
-# this is a Modular Arithmetic % operator problem
+# Count how many times the dial points to 0
 position = 50
-count = 0
+zero_count = 0
 
 with open('1_day.txt') as file:
     for line in file:
-        line = line.strip()
-        if line:
-            direction = line[0]
-            distance = int(line[1:])
-            position = (position - distance) % 100 if direction == 'L' else (position + distance) % 100
-            if position == 0:
-                count += 1
+        line = line.strip() # removes whitespace characters (spaces, tabs, newlines) from both the beginning and end of the string.
+        if not line:
+            continue
+            
+        direction = line[0] # get the rotation direction data
+        distance = int(line[1:]) # gets all characters from index 1 to the end (everything except the first character), then int() converts it to an integer. it is set at one as ) is the Lettre
 
-print(count)
+        
+        if direction == 'L':
+            position = (position - distance) % 100 # the % modulo operator gives us the remainder in relation tot he wrap arounrd and 100 is becuase there are 0-99 options
+        else:
+            position = (position + distance) % 100
+        
+        if position == 0:
+            zero_count += 1
+
+print(zero_count)
